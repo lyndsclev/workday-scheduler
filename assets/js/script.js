11ebe9cbd1a994c8654effe4cb38c32ba89a5f95
lyndsclev <lyndsclev@gmail.com>
2022-02-18 13:17:29 -0500

    got localStorage working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,39 @@
-// current day is displayed at the top of the calendar 
+// current day is displayed at top
 
-var today = moment().format("dddd, MMMM Do");
+var today = moment().format("dddd, MMMM, Do");
 $("#currentDay").append(today);
 
-// each timeblock is color-coded for past, present, or future 
+// color-code time block 
 
 var curHr = moment().hour();
 
-$("textarea").each(function(index, element) {
+$("textarea").each(function() {
 
-    if (this.id == curHr) {
+    if ($(this).parent().attr("id") == curHr) {
         $(this).addClass("present");
     }
-    else if (this.id < curHr) {
+    else if ($(this).parent().attr("id") < curHr) {
         $(this).addClass("past");
     }
-    else if (this.id > curHr) {
+    else if ($(this).parent().attr("id") > curHr) {
         $(this).addClass("future");
     };
-}); 
+    
+});
 
-// save task to local storage on saveBtn click 
+// get existing items from localStorage 
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+// save task to localStorage on saveBtn click 
 
 $(".saveBtn").on("click", function() {
 
@@ -29,6 +42,3 @@ $(".saveBtn").on("click", function() {
 
     localStorage.setItem(saveRow, task);
 });
-
-// get items from localStorage
-
